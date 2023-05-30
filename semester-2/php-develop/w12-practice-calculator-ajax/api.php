9268f20c6122071ac58e16796cd0fb66e1ffd3e7
pan93412 <pan93412@gmail.com>
2023-05-30 10:17:01 +0800

    s2-php: w12, w13: Typed Object

--- semester-2/php-develop/w12-practice-calculator-ajax/api.php
+++ semester-2/php-develop/w12-practice-calculator-ajax/api.php
@@ -1,10 +1,17 @@
 <?php
 
+class DecodedResult {
+    public int $left;
+    public string $op;
+    public int $right;
+}
+
 $input = file_get_contents('php://input');
+/** @var DecodedResult $data */
 $data = json_decode($input);
 
 $result = 0;
-eval("\$result = {$data->left} {$data->op} {$data->right};");
+eval("\$result = {$data->left} {$data->op} {$data->right};"); // hacky!
 
 echo json_encode([
     "ok" => true,
--- semester-2/php-develop/w13-score/api.php
+++ semester-2/php-develop/w13-score/api.php
@@ -1,6 +1,7 @@
 <?php
 
 $input = file_get_contents('php://input');
+/** @var int[] $data */
 $data = json_decode($input);
 
 $sum = array_sum($data) / count($data);
