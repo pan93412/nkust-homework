e18a6d8d07ab8b1f2271f2242230f2557e79b294
pan93412 <pan93412@gmail.com>
2023-10-26 00:58:02 +0800

    s3: service-side: Update models

--- semester-3/server-side/laravel-db/database/migrations/2023_10_24_065314_create_students_table.php
+++ semester-3/server-side/laravel-db/database/migrations/2023_10_24_065314_create_students_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('students', function (Blueprint $table) {
-            $table->id();
+            $table->uuid();
             $table->text("name");
             $table->text("addr");
             $table->date("birth");
