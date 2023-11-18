9e3dd2db285d08afc39467533e4931332273e661
pan93412 <pan93412@gmail.com>
2023-11-18 20:34:15 +0800

    s3: database: q25: update answer

--- semester-3/database/question/q25.sql
+++ semester-3/database/question/q25.sql
@@ -1,11 +1,11 @@
 -- 統計 2018 年各班各種假別的總數，產出缺曠課統計表
 
 select
-    students.ClassNo as 班別,
+    left(students.ClassNo, 3) as 班別,
     sum(records.Absent) as 曠課,
     sum(records.Leave) as 事假,
     sum(records.PublicLeave) as 公假,
     sum(records.SickLeave) as 病假
 from students join (records) using (ClassNo)
 where records.YMD between '2018-01-01' and '2018-12-31'
-group by students.ClassNo;
+group by 班別;
