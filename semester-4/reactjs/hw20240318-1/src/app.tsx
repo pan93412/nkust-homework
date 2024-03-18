257cef243ef7fc3566504b74018de281f8d4250f
pan93412 <pan93412@gmail.com>
2024-03-18 09:50:01 +0800

    s4: reactjs: hw20240318-1

---
+++ semester-4/reactjs/hw20240318-1/src/app.tsx
@@ -0,0 +1,13 @@
+import { Avatar, IntroCard } from "./components/intro.tsx";
+
+export function App() {
+  return (
+    <main>
+      <IntroCard>
+        <Avatar src="https://pan93.com/avatar.png" />
+        <h1>潘奕濬</h1>
+        <p>智商二甲 C111156103</p>
+      </IntroCard>
+    </main>
+  );
+}
---
+++ semester-4/reactjs/hw20240318-1/src/components/intro.tsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+export const IntroCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Avatar = styled.img`
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+`;
