1a0afcc1c19c640a0780b42f76cb3051f6cf85c7
pan93412 <pan93412@gmail.com>
2024-03-20 09:24:03 +0800

    s4: reactjs: hw20240318-2: complete

--- semester-4/reactjs/hw20240318-2/src/app.tsx
+++ semester-4/reactjs/hw20240318-2/src/app.tsx
@@ -1,8 +1,10 @@
 import { signal } from "@preact/signals";
 import { IntroCard, NegativeButton, PositiveButton, Title } from "./components/intro.tsx";
+import { StarIdentifier } from "./components/star.tsx";
 
 export function App() {
   const n = signal(0);
+  const star = signal(0);
 
   return (
     <main>
@@ -12,6 +14,10 @@ export function App() {
           <PositiveButton onClick={() => n.value++}>+1</PositiveButton>
           <NegativeButton onClick={() => n.value--}>-1</NegativeButton>
         </div>
+
+        <StarIdentifier activeStar={star} onSelectedStar={(selectedStar) => {
+          star.value = selectedStar
+        }} />
       </IntroCard>
     </main>
   );
