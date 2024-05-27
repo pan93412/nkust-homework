8a6c6cde570599eafec9f7b962b99a37a4bb30ae
pan93412 <pan93412@gmail.com>
2024-05-27 09:56:57 +0800

    s4: reactjs: hw20240527-2: init

---
+++ semester-4/reactjs/hw20240527-2/src/app.tsx
@@ -0,0 +1,51 @@
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import QRCode from "react-qr-code";
+
+const { Header, Content, Footer } = Layout;
+
+export function App() {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  const { t, i18n } = useTranslation();
+  const languages = ['en', 'ja', 'cn', 'tw'];
+  const [selectedLanguage, setSelectedLanguage] = useState(i18n.language);
+
+  useEffect(() => {
+    i18n.changeLanguage(selectedLanguage);
+  }, [i18n, selectedLanguage]);
+
+  return (
+    <Layout>
+      <Header style={{ display: 'flex', alignItems: 'center' }}>
+        <div className="demo-logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[selectedLanguage]} items={languages.map((l) => {
+          return { key: l, label: t(l) };
+        })} style={{ flex: 1, minWidth: 0 }} onSelect={(info) => setSelectedLanguage(info.key)} />
+      </Header>
+      <Content style={{ padding: '0 48px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }} items={[
+          {key: "home", title: t('home')},
+          {key: "list", title: t('list')},
+          {key: "app", title: t('app')},
+        ]} />
+        <div
+          style={{
+            background: colorBgContainer,
+            minHeight: 280,
+            padding: 24,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          <QRCode value="https://www.youtube.com/watch?v=dQw4w9WgXcQ" level="L" />
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        {t('copyright', { year: new Date().getFullYear() })}
+      </Footer>
+    </Layout>
+  );
+}
