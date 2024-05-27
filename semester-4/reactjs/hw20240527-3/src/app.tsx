55ab6639fdc06b67a124a2b1bdcbf9326dd1e1cf
pan93412 <pan93412@gmail.com>
2024-05-27 11:34:29 +0800

    s4: reactjs: hw20240527-3: basic form

--- semester-4/reactjs/hw20240527-3/src/app.tsx
+++ semester-4/reactjs/hw20240527-3/src/app.tsx
@@ -1,59 +1,39 @@
-import { Breadcrumb, Col, Flex, Layout, Menu, theme } from "antd";
-import { useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
-
-const { Header, Content, Footer } = Layout;
-
 export function App() {
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
-
-  const { t, i18n } = useTranslation();
-  const languages = ["en", "ja", "cn", "tw"];
-  const [selectedLanguage, setSelectedLanguage] = useState(i18n.language);
-
-  useEffect(() => {
-    i18n.changeLanguage(selectedLanguage);
-  }, [i18n, selectedLanguage]);
-
   return (
-    <Layout>
-      <Header style={{ display: "flex", alignItems: "center" }}>
-        <div className="demo-logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={[selectedLanguage]}
-          items={languages.map((l) => {
-            return { key: l, label: t(l) };
-          })}
-          style={{ flex: 1, minWidth: 0 }}
-          onSelect={(info) => setSelectedLanguage(info.key)}
-        />
-      </Header>
-      <Content style={{ padding: "0 48px" }}>
-        <Breadcrumb
-          style={{ margin: "16px 0" }}
-          items={[
-            { key: "home", title: t("home") },
-            { key: "list", title: t("list") },
-            { key: "app", title: t("app") },
-          ]}
-        />
-        <div
-          style={{
-            background: colorBgContainer,
-            minHeight: 280,
-            padding: 24,
-            borderRadius: borderRadiusLG,
-          }}
-        >
+    <main style={{
+      margin: "20px",
+      fontFamily: "sans-serif",
+    }}>
+      <form>
+        <div style={{
+          marginBottom: "12px",
+        }}>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" />
         </div>
-      </Content>
-      <Footer style={{ textAlign: "center" }}>
-        {t("copyright", { year: new Date().getFullYear() })}
-      </Footer>
-    </Layout>
+
+        <div style={{
+          marginBottom: "12px",
+        }}>
+        <label htmlFor="age">Age:</label>
+        <input type="number" id="age" name="age" min="1" max="200" />
+        </div>
+
+        <div style={{
+          marginBottom: "12px",
+        }}>
+          <label htmlFor="country">Country or Region:</label>
+          <select id="country" name="country">
+            <option value="taiwan">Taiwan</option>
+            <option value="usa">USA</option>
+            <option value="uk">UK</option>
+          </select>
+        </div>
+
+        <div>
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
+    </main>
   );
 }
