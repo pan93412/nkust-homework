fe8a4eb933fb364198e64ed3d5f51118dbe4500a
Yi-Jyun Pan <pan93412@gmail.com>
2024-09-24 11:16:49 +0800

    s5: w3-bundling-app: complete demo

--- semester-5/app/w3-bundling-app-apk/app/index.tsx
+++ semester-5/app/w3-bundling-app-apk/app/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
+  const name = "潘奕濬";
+  const sid = "C111156103";
+  const department = "智慧商務系";
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{name}</Text>
+      <Text>{formatTitle(sid, department)}</Text>
     </View>
   );
 }
+
+export function formatTitle(sid: string, department: string): string {
+  return `${sid} ${department}`
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 36,
+    color: "darkblue",
+    fontWeight: 'bold',
+  },
+});
