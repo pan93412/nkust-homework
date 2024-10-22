8d988136a5ba66b0c894f398ea86dfffb297733a
Yi-Jyun Pan <pan93412@gmail.com>
2024-10-22 13:56:11 +0800

    s5: app: w7-app: update

--- semester-5/app/w7-app/app/index.tsx
+++ semester-5/app/w7-app/app/index.tsx
@@ -1,15 +1,52 @@
-import { Text, View } from "react-native";
+import { ReactNode, useState } from "react";
+import { ActivityIndicator, Switch, Text, View } from "react-native";
 
 export default function Index() {
+  const [switchOp, setSwitchOp] = useState(false);
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        gap: 8,
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <ComponentPractice fontSize={36} onPress={() => alert(`Hello, ${switchOp ? 'world' : 'owo'}!`)}>
+        Hello, World!
+      </ComponentPractice>
+
+      <Switch value={switchOp} onValueChange={setSwitchOp} />
+
+      <ActivityIndicator />
+      <ActivityIndicator size="large" />
+      <ActivityIndicator size="small" />
     </View>
+  );
+}
+
+export interface ComponentPracticeProps {
+  children: ReactNode;
+  onPress: () => void;
+  fontSize?: number;
+}
+
+export function ComponentPractice({
+  fontSize = 20,
+  ...props
+}: ComponentPracticeProps) {
+  return (
+    <Text
+      style={{
+        color: "red",
+        fontSize: fontSize,
+        fontWeight: "bold",
+        textAlign: "center",
+      }}
+      onPress={props.onPress}
+    >
+      {props.children}
+    </Text>
   );
 }
