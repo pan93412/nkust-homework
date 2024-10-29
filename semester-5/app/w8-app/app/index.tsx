58aeebbc744064057c7117355f86ca38e8863124
Yi-Jyun Pan <pan93412@gmail.com>
2024-10-29 09:45:05 +0800

    s5: app: w8-app: Modal

--- semester-5/app/w8-app/app/index.tsx
+++ semester-5/app/w8-app/app/index.tsx
@@ -1,5 +1,5 @@
 import { ReactNode, useState } from "react";
-import { ActivityIndicator, Switch, Text, View } from "react-native";
+import { ActivityIndicator, Modal, Switch, Text, View } from "react-native";
 
 export default function Index() {
   const [switchOp, setSwitchOp] = useState(false);
@@ -13,6 +13,13 @@ export default function Index() {
         gap: 8,
       }}
     >
+      <Modal visible={switchOp} animationType="slide">
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <Text>Modal</Text>
+          <Switch value={switchOp} onValueChange={setSwitchOp} />
+        </View>
+      </Modal>
+
       <ComponentPractice fontSize={36} onPress={() => alert(`Hello, ${switchOp ? 'world' : 'owo'}!`)}>
         Hello, World!
       </ComponentPractice>
