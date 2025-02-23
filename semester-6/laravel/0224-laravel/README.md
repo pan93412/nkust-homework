246e245d5e6828c21c98c1c16faacceea0981549
Yi-Jyun Pan <me@pan93.com>
2025-02-24 01:57:02 +0800

    s6: laravel: 2024-02-24: initialize

---
+++ semester-6/laravel/0224-laravel/README.md
@@ -0,0 +1,3 @@
+# hw1
+
+![](./laravel.png)
---
+++ semester-6/laravel/0224-laravel/devenv.nix
@@ -0,0 +1,48 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git pkgs.laravel ];
+
+  # https://devenv.sh/languages/
+  languages.php.enable = true;
+  languages.php.version = "8.3";
+  languages.javascript.enable = true;
+  languages.javascript.pnpm.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
