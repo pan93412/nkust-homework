e9823826ab9748f686fac9562b0459254e221a19
Yi-Jyun Pan <me@pan93.com>
2025-02-24 10:26:35 +0800

    s6: laravel: 0224-laravel: hw2

---
+++ semester-6/laravel/0224-laravel/resources/views/user-list.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
+</head>
+<body>
+    <div id="app">
+        <h1>User List</h1>
+        <ul>
+            @foreach ($users as $user)
+                <li>{{ $user->name }}</li>
+            @endforeach
+        </ul>
+    </div>
+</body>
+</html>
--- semester-6/laravel/0224-laravel/routes/web.php
+++ semester-6/laravel/0224-laravel/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users', [UserController::class, 'index']);
