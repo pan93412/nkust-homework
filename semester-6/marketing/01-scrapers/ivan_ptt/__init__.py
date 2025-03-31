9cd7cec8b5c825111ae667f30d1b37ee478fe6ba
Yi-Jyun Pan <me@pan93.com>
2025-03-31 10:39:55 +0800

    s6: marketing: 03-ptt-packages: implement ivan_ptt_02

--- semester-6/marketing/01-scrapers/ivan_ptt/__init__.py
+++ semester-6/marketing/01-scrapers/ivan_ptt/__init__.py
@@ -1,1 +1,4 @@
-from .Ivan_ptt import crawl_ptt_page as crawl_ptt_page
+from .ivan_ptt_01 import crawl_ptt_page as crawl_ptt_page_01
+from .ivan_ptt_02 import crawl_ptt_page as crawl_ptt_page_02
+
+__all__ = ["crawl_ptt_page_01", "crawl_ptt_page_02"]
