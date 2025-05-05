28f5885d2b56ea5abf18c0b09d989109d728022e
Yi-Jyun Pan <me@pan93.com>
2025-05-05 10:38:48 +0800

    s6: laravel: 0504-laravel-api: done

---
+++ semester-6/laravel/0504-laravel-api/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/test', function () {
+    return response()->json([
+        'message' => 'It works (in /test)!',
+    ]);
+});
