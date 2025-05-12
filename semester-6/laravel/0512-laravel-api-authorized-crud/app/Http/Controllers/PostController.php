59905c870fd1b02216333b84d1cd479b91b739f5
Yi-Jyun Pan <me@pan93.com>
2025-05-12 11:51:52 +0800

    s6: laravel: 0512-laravel-api-authorized-crud: add post controller

---
+++ semester-6/laravel/0512-laravel-api-authorized-crud/app/Http/Controllers/PostController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return Post::all();
+    }
+
+    public function store(Request $request)
+    {
+        return Post::create($request->all());
+    }
+
+    public function show(int $id)
+    {
+        return Post::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $post = Post::findOrFail($id);
+        $post->update($request->all());
+        return $post;
+    }
+
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+        return 204;
+    }
+}
